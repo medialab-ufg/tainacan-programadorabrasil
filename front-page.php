f059226eeedb6ec46a01b210d0be3e9dfa258764
Fabiano <fabianocarvalho15@gmail.com>
2018-10-30 15:05:41 -0300

    Transfom in functions to display carousel on front page

--- front-page.php
+++ front-page.php
@@ -5,31 +5,8 @@
 <!-- Get the menu if is create in panel admin -->
 <?php get_template_part( 'template-parts/menubellowbanner' ); ?>
 
-<?php
-$collection = \Tainacan\Repositories\Collections::get_instance()->fetch_one(['name' => 'Catalogo de Filmes'], 'OBJECT');
-$items = \Tainacan\Repositories\Items::get_instance()->fetch(['orderby' => 'post_title', 'order' => 'ASC', 'nopaging' => true], $collection);
-?>
+<?php carousel_front_page('Collection test', 'Collection test'); ?>
 
-<?php if ($items->have_posts()): ?>
-	<div class="container-fluid max-large px-0 margin-top-95">
-		<div id="content" role="main" class="margin-one-column">
-			<h3 class="mb-4">
-				<a href="#" class="font-weight-bold">Edições da Revista</a>
-			</h3>
-			<ul class="carrossel-revistas">
-
-			<?php while ($items->have_posts()): $items->the_post(); ?>
-				<li>
-					<a href="<?php the_permalink(); ?>">
-						<?php the_post_thumbnail('tainacan-medium-full'); ?>
-						<span><?php the_title(); ?></span>
-					</a>
-				</li>
-			<?php endwhile; ?>
-
-			</ul>
-		</div>
-	</div>
-<?php endif; ?>
+<?php carousel_front_page('Collection test 1', 'Collection test 1'); ?>
 
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -17,3 +17,35 @@ function carousel_files() {
     wp_enqueue_script('call-carousel', get_theme_file_uri('/assets/js/base.js'), null, microtime(), true);
 }
 add_action('wp_enqueue_scripts','carousel_files');
+
+/**
+ * Display carousel of collection
+ * @param 1 Collection name | Get the items of collections
+ * @param 2 Title of carousel
+ */
+function carousel_front_page($collection_name, $title_carousel) {
+    $collection = \Tainacan\Repositories\Collections::get_instance()->fetch_one(['name' => $collection_name], 'OBJECT');
+    $items = \Tainacan\Repositories\Items::get_instance()->fetch(['orderby' => 'post_title', 'order' => 'ASC', 'nopaging' => true], $collection);
+
+    if ($items->have_posts()): ?>
+        <div class="container-fluid max-large px-0 margin-top-95">
+            <div id="content" role="main" class="margin-one-column">
+                <h3 class="mb-4">
+                    <a href="#" class="font-weight-bold"><?php echo $title_carousel; ?></a>
+                </h3>
+                <ul class="carrossel-revistas">
+
+                <?php while ($items->have_posts()): $items->the_post(); ?>
+                    <li>
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail('tainacan-medium'); ?>
+                            <span><?php the_title(); ?></span>
+                        </a>
+                    </li>
+                <?php endwhile; ?>
+
+                </ul>
+            </div>
+        </div>
+    <?php endif;
+}
