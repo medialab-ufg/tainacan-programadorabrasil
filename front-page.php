a6b2f5d6cdee93a6e0a7d23477914f33e479678d
Fabiano <fabianocarvalho15@gmail.com>
2018-11-13 16:24:53 -0200

    Change de collection names

--- front-page.php
+++ front-page.php
@@ -5,8 +5,10 @@
 <!-- Get the menu if is create in panel admin -->
 <?php get_template_part( 'template-parts/menubellowbanner' ); ?>
 
-<?php carousel_front_page('Collection test', 'Collection test'); ?>
+<?php carousel_front_page('Catalogo de Filmes', 'Catalogo de Filmes'); ?>
 
-<?php carousel_front_page('Collection test 1', 'Collection test 1'); ?>
+<?php carousel_front_page('Filmes Edital Carmen Santos
+', 'Filmes Edital Carmen Santos
+'); ?>
 
 <?php get_footer(); ?>
