c1c360d9cb0d0cf4513cae36aa0a620496aa78c3
Fabiano <fabianocarvalho15@gmail.com>
2018-11-14 11:11:24 -0200

    Change order of collection

--- front-page.php
+++ front-page.php
@@ -5,10 +5,10 @@
 <!-- Get the menu if is create in panel admin -->
 <?php get_template_part( 'template-parts/menubellowbanner' ); ?>
 
-<?php carousel_front_page('Catalogo de Filmes', 'Catalogo de Filmes'); ?>
-
 <?php carousel_front_page('Filmes Edital Carmen Santos
 ', 'Filmes Edital Carmen Santos
 '); ?>
+
+<?php carousel_front_page('Catalogo de Filmes', 'Catalogo de Filmes'); ?>
 
 <?php get_footer(); ?>
